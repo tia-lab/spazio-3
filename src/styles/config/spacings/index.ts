a0bd8b6ef9701a95deaa50b4551b58b10202c602
tia-lab <tia@eki.studio>
2024-10-21 15:13:34 +0200

    [18] fix: update page width in spacings configuration

--- src/styles/config/spacings/index.ts
+++ src/styles/config/spacings/index.ts
@@ -22,7 +22,7 @@ export const layoutSpacingVars = {
   base: '16',
 
   containerGutter: { default: '1.5rem', [MEDIA.tablet]: '1rem' },
-  pageWidth: '2200px',
+  pageWidth: '2500px',
   padding: {
     global: { default: '1.5rem', [MEDIA.tablet]: '1rem' },
     vertical: {
